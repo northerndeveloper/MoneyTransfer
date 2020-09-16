97a801a580b1d651709dbe78e452172095254896
Alper Kopuz <alper.kopuz@easy-software.com>
2020-09-16 22:29:10 +0300

    pre-commit

--- src/main/java/MoneyTransferMainApplication.java
+++ src/main/java/MoneyTransferMainApplication.java
@@ -4,7 +4,13 @@ import services.WebServiceServices;
 import services.impl.GeneralServicesImpl;
 import services.impl.WebServiceServicesImpl;
 
-/** Created by kopuz on 7/10/2018. */
+/** Created by kopuz on 7/1
+ *
+ *
+ *
+ *
+ *
+ * 0/2018. */
 public class MoneyTransferMainApplication {
 
   public static void main(String[] args) throws TransferMoneyException {
@@ -12,10 +18,13 @@ public class MoneyTransferMainApplication {
     /** First it creates necessary services to send money */
     GeneralServices generalServices = new GeneralServicesImpl();
 
-         generalServices.generateEntitiesForMoneyTransfer();
+           generalServices.generateEntitiesForMoneyTransfer();
 
     /** Then it sends money by webservice */
               WebServiceServices webServiceServices = new WebServiceServicesImpl();
-    webServiceServices.getMoneyTransfer();
+
+
+
+             webServiceServices.getMoneyTransfer();
   }
 }
